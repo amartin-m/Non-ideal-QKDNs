2707c8d24dd3e691481a9e7f4e9d7a2e83f76b34
amartin-m <martinmeginoa@gmail.com>
2025-05-29 16:35:59 +0200

    Update qkd_node.py

--- nodes/qkd_node.py
+++ nodes/qkd_node.py
@@ -1,7 +1,3 @@
-import sys
-sys.path.append('/Users/andres/Documents/VisualStudio/BB84_Project/nodes')
-
-
 from typing import Dict
 from qkd_link_informer import QKDLinkInformer
 from netsquid.nodes import Node
